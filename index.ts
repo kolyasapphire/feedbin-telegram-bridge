7b35cfc0f4e4fb0fa102ef77c61feec9060a63d5
kolyasapphire <a@ks.gg>
2024-11-02 20:49:17 +0000

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,75 @@
+import { encodeBase64 } from 'jsr:@std/encoding/base64'
+
+import type { Entry } from './types.ts'
+
+const API_BASE = 'https://api.feedbin.com/v2'
+
+const FEEDBIN_BASE_AUTH = Deno.env.get('FEEDBIN_BASE_AUTH') // user:password
+const BOT_TOKEN = Deno.env.get('BOT_TOKEN')
+const BOT_CHAT = Deno.env.get('BOT_CHAT')
+
+const job = async () => {
+  if (!FEEDBIN_BASE_AUTH || !BOT_TOKEN || !BOT_CHAT) {
+    console.error('Bad config')
+    return
+  }
+
+  const sendMessage = async (
+    text: string,
+    options?: { [key: string]: unknown },
+  ) => {
+    const body = {
+      chat_id: BOT_CHAT,
+      text,
+      ...options,
+    }
+
+    const req = await fetch(
+      `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      },
+    )
+
+    if (!req.ok) console.error(await req.json())
+  }
+
+  const headers = {
+    'Authorization': `Basic ${encodeBase64(FEEDBIN_BASE_AUTH)}`,
+    'Content-Type': 'application/json; charset=utf-8', // required
+  }
+
+  // biome-ignore lint: it's better
+  const unreadRes = await fetch(API_BASE + '/unread_entries.json', { headers })
+  const unreadIds = await unreadRes.json() as number[]
+
+  // biome-ignore lint: it's better
+  const entriesRes = await fetch(API_BASE + `/entries.json?ids=${unreadIds.join(',')}`, { headers })
+  const entries = await entriesRes.json() as Entry[]
+
+  for (const entry of entries) {
+    await sendMessage(
+      [
+        entry.title,
+        '', // spacer
+        entry.url,
+      ].join('\n'),
+      { parse_mode: 'HTML' },
+    )
+  }
+
+  // biome-ignore lint: it's better
+  await fetch(API_BASE + '/unread_entries.json', {
+    method: 'DELETE',
+    headers,
+    body: JSON.stringify({
+      'unread_entries': unreadIds,
+    }),
+  })
+}
+
+Deno.cron('job', '*/15 * * * *', job)
---
+++ types.ts
@@ -0,0 +1,12 @@
+export type Entry = {
+  id: number // 4610179952
+  feed_id: number // 2276854
+  title: string // 'Possible futures of the Ethereum protocol, part 6: The Splurge'
+  author: null
+  summary: string // ''
+  content: null
+  url: string // 'https://vitalik.ca/general/2024/10/29/futures6.html'
+  extracted_content_url: string // 'https://extract.feedbin.com/parser/feedbin/4e0370d3a141024cb936651178794929c6e4acc4?base64_url=aHR0cHM6Ly92aXRhbGlrLmNhL2dlbmVyYWwvMjAyNC8xMC8yOS9mdXR1cmVzNi5odG1s'
+  published: string // '2024-10-29T00:00:00.000000Z'
+  created_at: string // '2024-10-29T00:45:58.880956Z'
+}
